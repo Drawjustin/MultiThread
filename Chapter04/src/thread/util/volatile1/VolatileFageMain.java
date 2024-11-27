269675ccde2f43c2334028e3bb43a952a33b3159
유현종 <guswhd903@naver.com>
2024-11-27 15:22:34 +0900

    ADD : 메모리 가시성, CONTENT : volatile, 메모리가시성, JVM, 캐시메모리

---
+++ Chapter04/src/thread/util/volatile1/VolatileFageMain.java
@@ -0,0 +1,33 @@
+package thread.util.volatile1;
+
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class VolatileFageMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlage = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlage를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlage = " + task.runFlag);
+        log("main 종료");
+    }
+    static class MyTask implements Runnable{
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while(runFlag){
+                // runFlag가 false로 변하면 탈출
+
+            }
+            log("task 종료");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MultiThread
 A practice platform for exploring and understanding multi-threading features in Java 21
+
+김영한의 MultiThread 강의 정리입니다 !
+https://www.inflearn.com/course/%EA%B9%80%EC%98%81%ED%95%9C%EC%9D%98-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-%EA%B3%A0%EA%B8%89-1?attributionToken=lQHwlAoMCMjlmroGEMHjxe0CEAEaJDY3NWVjMWMyLTAwMDAtMjk0MS04YTQ4LTNjMjg2ZDQ3MmRjMioHMTI1MDU3MjI4t7eMLajlqi2jgJcimNa3LY6-nRXFy_MXwvCeFdSynRWa7sYwnN3YMJD3sjCb1rctmd3YMI6RyTA6DmRlZmF1bHRfc2VhcmNoSAFYAWABaAF6AnNp
